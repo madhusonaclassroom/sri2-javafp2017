a11663df38c7613f50511723332a0c6934d3c462
javafp2017 <61223058+javafp2017@users.noreply.github.com>
2021-07-29 16:14:00 +0530

    Update Hello.java

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -2,8 +2,9 @@ package com.example.project;
 
 public class Hello {
 
-	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
+	public int add(int a,int b)
+	{
+		return a+b;
 	}
 
 }
